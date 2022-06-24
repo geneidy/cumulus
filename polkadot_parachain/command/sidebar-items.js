81a434263839c248638c2916a040f7408fb04b0e
geneidy <geneidy@users.noreply.github.com>
2022-06-24 03:24:37 +0000

    deploy: 4bb9b610f46c69438cbd525490309191c76726e1

---
+++ polkadot_parachain/command/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Runtime",""]],"fn":[["load_spec",""],["run","Parse command line arguments into service configuration."],["runtime",""]],"macro":[["construct_async_run",""],["construct_benchmark_partials","Creates partial components for the runtimes that are supported by the benchmarks."]],"trait":[["ChainType",""]]});
